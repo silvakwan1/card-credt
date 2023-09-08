9936e2ff522eeed4ea4a3e21a79cdd52988bc06c
silvakwan1 <123783319+silvakwan1@users.noreply.github.com>
2023-09-08 04:21:14 -0300

    upgrade

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
 import cardValidator from "./index.js";
+
 const cardNumber = document.querySelector('#card-value');
-const text = document.querySelector("#number-card");
+const dataCard = document.querySelector("#data-card");
+const cvvCard =document.querySelector("#cvv-card");
+const nameCard =document.querySelector("#name-card");
 
-cardNumber.addEventListener("input", () => {
-    text.innerHTML = cardNumber.value;
-})
+dataCard.addEventListener("input", renderDataCard)
 
-cardNumber.addEventListener("blur", () => {
-    let border =(cardValidator(cardNumber.value) ? "2.5px white solid" : "2.5px red solid")
+nameCard.addEventListener("input", renderNameCard)
+
+cvvCard.addEventListener("input", renderCvvCard)
+//console.log("oi")
+
+cardNumber.addEventListener("input", renderCardNumber)
+
+cardNumber.addEventListener("blur", verificationCardNumber)
+function renderDataCard () {
+    const text = document.querySelector(".expiration");
+    dataCard.value = dataCard.value.replace(/[^0-9\s/]/g, '')
+    text.innerHTML = dataCard.value;
+}
+function renderNameCard () {
+    const textName = document.querySelector(".name");
+    nameCard.value = nameCard.value.replace(/[^a-zA-Z\s]/g, '')
+    textName.innerHTML = nameCard.value; 
+}
+function renderCvvCard () {
+    const textCvv = document.querySelector(".cvv");
+    cvvCard.value = cvvCard.value.replace(/[^0-9\s/]/g, '')
+    textCvv.innerHTML = cvvCard.value;
+}
+
+function renderCardNumber(){
+    const textNumber = document.querySelector("#number-card");
+    cardNumber.value = cardNumber.value.replace(/[^0-9\s/]/g, '')
+    let newCard = cardNumber.value;
+
+    if (!newCard.includes(" ")) {
+
+        let formattedCard = "";
+        for (let i = 0; i < newCard.length; i += 4) {
+            formattedCard += newCard.slice(i, i + 4) + " ";
+        }
+        formattedCard = formattedCard.trim();
+
+        textNumber.innerHTML = formattedCard;
+    } else {
+        textNumber.innerHTML = cardNumber.value;
+    }
+}
+
+
+function verificationCardNumber () {
+    let newCardNumber = cardNumber.value;
+
+    if (newCardNumber.includes(" ")) {
+
+        while (newCardNumber.includes(" ")) {
+            newCardNumber = newCardNumber.replace(" ", "");
+        }
+    }
+ //   console.log(newCardNumber)
+
+    let border =(cardValidator(newCardNumber) ? "2.5px white solid" : "2.5px red solid")
     cardNumber.style.border = border; 
-})
+};
