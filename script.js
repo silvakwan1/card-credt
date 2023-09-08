3314fa6cb2f6ba7c6ab719a6190c7801ab5bba0c
silvakwan1 <123783319+silvakwan1@users.noreply.github.com>
2023-09-08 01:25:34 -0300

    arrumei algumas coisas

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 import cardValidator from "./index.js";
-const input = document.querySelector('#card-value');
-let cartao = input.value
-let newCard = ""
-input.addEventListener("keyup", formatar)
-function formatar(){
-    let i = cartao.length
-    let str = "0000 0000 0000 0000"
-    if (i <= str.length){
-        newCard += str[i]
-    }
-    console.log(str[i])
-}
+const cardNumber = document.querySelector('#card-value');
+const text = document.querySelector("#number-card");
 
+cardNumber.addEventListener("input", () => {
+    text.innerHTML = cardNumber.value;
+})
 
-
-// const text = document.querySelector("#card-number");
-// 
-
-
-// input.addEventListener("input", () => {
-//     text.innerHTML = input.value;
-// })
-// input.addEventListener("blur", () => {
-//     let border =(cardValidator(input.value) ? "2.5px white solid" : "2.5px red solid");
-
-//     input.style.border = border; 
-//     // input.style.boxShadow = "0
-// })
+cardNumber.addEventListener("blur", () => {
+    let border =(cardValidator(cardNumber.value) ? "2.5px white solid" : "2.5px red solid")
+    cardNumber.style.border = border; 
+})
--- text.js
+++
@@ -1,6 +0,0 @@
-let julio = "ekssss gay"
-let gay = "gay"
-
-let sxcvvn = (julio.length > gay.length ? "ele e gay" : " ele nao e")
-
-console.log(sxcvvn);
